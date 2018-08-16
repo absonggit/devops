824dae82adabe75011116b51c0dfde847fed4192
absong <absong@absong.com>
2018-08-16 15:25:32 +0800

    update

--- gitlab/install_gitlab.sh
+++ gitlab/install_gitlab.sh
@@ -2,7 +2,7 @@
 yum install -y curl policycoreutils-python openssh-server postfix
 systemctl enable postfix
 systemctl start postfix
-curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
-yum install -y gitlab-ce
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
+yum install -y gitlab-ee
 gitlab-ctl reconfigure
 gitlab-ctl restart
