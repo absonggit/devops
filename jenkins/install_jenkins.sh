932e354bb342c8b9b17b9021649f723c4acab698
absong <absong@absong.com>
2018-08-20 10:19:15 +0800

    update Jenkins

--- jenkins/install_jenkins.sh
+++ jenkins/install_jenkins.sh
@@ -4,4 +4,4 @@ curl https://pkg.jenkins.io/redhat/jenkins.repo >> /etc/yum.repos.d/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 yum install java jenkins -y
 sed -i 's/JENKINS_USER="jenkins"/JENKINS_USER="root"/g' /etc/sysconfig/jenkins
-systectl restart jenkins
+systemctl restart jenkins
